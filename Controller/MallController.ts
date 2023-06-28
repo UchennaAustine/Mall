7e8e60dbd82f96eee3c64652b3d25a28c2e3e304
UchennaAustine <uchennaaustine8@gmail.com>
2023-06-28 12:25:08 +0100

    extra

--- Controller/MallController.ts
+++ Controller/MallController.ts
@@ -4,7 +4,7 @@ import { IMall, MallModel } from "../Model/MallModel";
 export const viewAllProducts = async (req: Request, res: Response) => {
   try {
     const products = await MallModel.find();
-    if (products.length >= 0) {
+    if (products.length <= 0) {
       res.status(404).json({
         message: "No Available Products For Now",
       });
@@ -43,12 +43,12 @@ export const stockProducts = async (
   try {
     const { name, description, price, image, category, brand, inStock } =
       req.body;
-      const Image = brand?.charAt(0).toUpperCase()
+    const Image = brand?.charAt(0).toUpperCase();
     const newProduct = await MallModel.create({
       name,
       description,
       price,
-      image:Image,
+      image: Image,
       category,
       brand,
       inStock,
@@ -59,6 +59,30 @@ export const stockProducts = async (
     });
   } catch (error: any) {
     //Internal Server error status code = 500
+    return res.status(500).json(error.message);
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, description, price, image, inStock } = req.body;
+    const Image = name?.charAt(0).toUpperCase();
+    const productUpdate = await MallModel.findByIdAndUpdate(
+      req.params.productID,
+      {
+        name,
+        description,
+        price,
+        image: Image,
+        inStock,
+      },
+      { new: true }
+    );
+    return res.status(201).json({
+      message: "This is the Latest Update to this Product",
+      data: productUpdate,
+    });
+  } catch (error: any) {
     return res.status(500).json(error.message);
   }
 };
